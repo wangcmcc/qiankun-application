615d0ab2570a60889bbd0eee1a8b211a5ed0f20d
wangyangwywywwy8 <7743616+wangyangwywywwy8@user.noreply.gitee.com>
2020-10-30 22:41:06 +0800

    添加基座

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <el-menu :el-menu-item="true" mode=horizontal>
+       <!-- 基座中可以放自己的路由 -->
+       <el-menu-item index="/">Home</el-menu-item> 
+       <!-- 引用其他子应用 -->
+       <el-menu-item index="/vue">Vue应用</el-menu-item> 
+       <el-menu-item index="/react">React应用</el-menu-item> 
+    </el-menu>
+    <router-view></router-view>
+    <div id='vue'></div>
+    <div id='react'></div>
+      
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
 
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
+import {registerMicroApps, start} from 'qiankun'
+
+const apps = [
+  {
+    name: 'vueApp',
+    entry: '//localhost:10000',
+   // fetch
+    container: '#vue',
+    activeRule: '/vue'
+  },
+  {
+    name: 'reactApp',
+    entry: '//localhost:20000',
+   // fetch
+    container: '#react',
+    activeRule: '/react'
+  }
+]
+registerMicroApps(apps)
+start()
 new Vue({
   router,
   store,
